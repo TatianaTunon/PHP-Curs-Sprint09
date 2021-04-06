45948802a7de30215b15d00dea1c9c54455eef6e
TatianaTunon <76038315+TatianaTunon@users.noreply.github.com>
2021-04-06 16:25:39 +0200

    Update BienvenidoController.php
    Cambio: La variable debe de enviarse a través de la ruta para posteriormente ser mostrada en la vista.

--- app/Http/Controllers/BienvenidoController.php
+++ app/Http/Controllers/BienvenidoController.php
@@ -6,9 +6,9 @@ use Illuminate\Http\Request;
 
 class BienvenidoController extends Controller
 {
-    public function vista1 (){
-        $data['nom']="Cassandra";
-        return view ('Bienvenido',$data);
+     public function vista1 ($nom){
+        //$data['nom']="Cassandra";
+        return view ('Bienvenido',$nom);
         //echo "estoy pasando por el controlador"; 
         //return view ('vista1');
     }
